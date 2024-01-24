d8b2fa353fc2e2a81ab0239415a4ef8fec9bd7ad
Marcel Mir <135972715+Marc1MT@users.noreply.github.com>
2024-01-24 21:18:31 +0000

    go

--- streamlit2.py
+++ streamlit2.py
@@ -12,31 +12,13 @@ def predict_imagen(imagen, model):
     class_names = ['COVID', 'SANO', 'PNEUMONIA', 'TUBERCULOSIS']
     return class_names[predicted_class]
 
-#def predict_imagen(imagen):
-    imagen = imagen.reshape((1, 150, 150, 3))
-    #pred_covid = model_covid.predict(imagen)[0][0]
-    #pred_sano = model_sano.predict(imagen)[0][0]
-    #pred_pneumonia = model_pneumonia.predict(imagen)[0][0]
-    #pred_tuberculosis = model_tuberculosis.predict(imagen)[0][0]
-    # Crea un diccionario de predicciones
-    #predictions = {
-        #'COVID': pred_covid,
-        #'SANO': pred_sano,
-        #'PNEUMONIA': pred_pneumonia,
-        #'TUBERCULOSIS': pred_tuberculosis
-    #}
-   # closest_to_zero = min(predictions, key=predictions.get) # Selecciona la categoría con el resultado más cercano a 0
-    #return closest_to_zero
-
 def main():
-    st.markdown("<h1 style='text-align: center; color: black; font-size: 40px;'>Servicio de Neumología de 4geeks</h1>", unsafe_allow_html=True)
+    st.title('Bienvenido al servicio de neumología de 4geeks')
     st.write('**Por favor tenga sus radiografias a mano (en el ordenador)**')
     
     uploaded_file = st.file_uploader("Cargar imagen", type=["jpg", "jpeg", "png"])
-    
     #Carga el modelo
-    model = load_model('/workspaces/lungsx-ray/modelo_softmax_optimizado.keras')
-
+    model = load_model('models/modelo_softmax_optimizado_contraste.keras')
 
     if uploaded_file is not None:
 
@@ -51,6 +33,7 @@ def main():
         image.thumbnail((299, 299), Image.BICUBIC)  # Use BICUBIC instead of ANTIALIAS
 
 
+
         if image is not None:
             image = Image.open(uploaded_file).resize((150, 150))
             st.image(image, caption='Imagen cargada', use_column_width=True)
@@ -58,12 +41,10 @@ def main():
             image = np.array(image) / 255.0
 
             if st.button('Realizar Predicción de la categoría de la imagen'):
-                pred = predict_imagen(image)
-
+                pred = predict_imagen(image, model)
                 st.success('Éxito al realizar la predicción!')
                 st.write('La categoría predicha para la imagen:')
                 st.write(pred)
-                st.write('Por favor contraste los resultados con un profesional')
     
 if __name__ == "__main__":
     main()
